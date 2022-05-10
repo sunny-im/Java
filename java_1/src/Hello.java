bd5208cc88bdd7fde8ef6cf2e00ecf7a94eb074e
sunny-im <hellossun.dahae@gmail.com>
2022-05-10 23:46:22 +0900

    java_1

--- java_1/src/Hello.java
+++ java_1/src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println(1+2);
+        int percent = 13;
+
+        System.out.println(100*percent/100.0);
+        System.out.println(300*percent/100.0);
+        System.out.println(1000*percent/100.0);
+        System.out.println(10000*percent/100.0);
     }
 }
