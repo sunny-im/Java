48dc0397747df36c0497f3703f445bc31a5f5fa0
sunny-im <hellossun.dahae@gmail.com>
2022-05-14 11:57:08 +0900

    _

---
+++ java_1/src/fileInput.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class fileInput {
+    public static void main(String[] args) {
+        FileInputStream inputStream = null;
+
+        try {
+            inputStream = new FileInputStream("src/hako.txt");
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+        String line2 = sc.nextLine();
+        System.out.println(line2);
+    }
+}
