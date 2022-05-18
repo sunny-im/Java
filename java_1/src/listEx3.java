708786dd1dbffb0e58a78d534f2cc71922012964
sunny-im <hellossun.dahae@gmail.com>
2022-05-18 23:07:04 +0900

    listEx3

---
+++ java_1/src/listEx3.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class listEx3 {
+    public static void main(String[] args) {
+        List<Integer> num = Arrays.asList(1,5,4,2,6,7);  // 배열을 리스트로 변환
+        System.out.println(num); // [1, 5, 4, 2, 6, 7]
+    }
+}
